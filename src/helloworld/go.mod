c6e71f0534082266c81e959fb9d2eaf9952461aa
田昆 <1172504039@qq.com>
2023-08-18 17:36:39 +0800

    多线程和通道

--- src/helloworld/go.mod
+++ src/helloworld/go.mod
@@ -3,6 +3,9 @@ module helloworld
 go 1.19
 
 
-require github.com/Darcula666/calculator v0.0.0
+require (
+github.com/Darcula666/calculator v0.1.0
+rsc.io/quote v1.5.2
+)
 
 replace github.com/Darcula666/calculator => ../calculator
--- src/helloworld/main.go
+++ src/helloworld/main.go
@@ -3,15 +3,69 @@ package main
 
 import (
 	"fmt"
-	"github.com/Darcula666/calculator"
+	"net/http"
+	"time"
 )
 
+func highlow(high int, low int) {
+	if high < low {
+		fmt.Println("Panic!")
+		panic("highlow() low greater than high")
+	}
+	defer fmt.Println("Deferred: highlow(", high, ",", low, ")")
+	fmt.Println("Call: highlow(", high, ",", low, ")")
+
+	highlow(high, low+1)
+}
 func main() {
-	firstName := "John"
-	updateName(&firstName)
-	fmt.Println(firstName)
-	total := calculator.Sum(3, 5)
-	fmt.Println(total)
+	start := time.Now()
+	apis := []string{
+		"https://management.azure.com",
+		"https://dev.azure.com",
+		"https://api.github.com",
+		"https://outlook.office.com/",
+		"https://api.somewhereintheinternet.com/",
+		"https://graph.microsoft.com",
+	}
+	ch := make(chan string)
+	for _, api := range apis {
+		go checkAPI(api, ch)
+	}
+
+	for i := 0; i < len(apis); i++ {
+		fmt.Print(<-ch)
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("Done! It took %v seconds!\n", elapsed.Seconds())
+}
+
+func checkAPI(api string, ch chan string) {
+	_, err := http.Get(api)
+	if err != nil {
+		ch <- fmt.Sprintf("ERROR: %s is down!\n", api)
+		return
+	}
+	ch <- fmt.Sprintf("SUCCESS: %s is up and running!\n", api)
+}
+
+type Employee struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Address   string
+}
+
+func getInformation(id int) (*Employee, error) {
+	employee, err := apiCallEmployee(1000)
+	if err != nil {
+		return nil, fmt.Errorf("Got an error when getting the employee information: %v", err)
+	}
+	return employee, nil
+}
+
+func apiCallEmployee(id int) (*Employee, error) {
+	employee := Employee{LastName: "Doe", FirstName: "John"}
+	return &employee, nil
 }
 
 func updateName(name *string) {
